bd9fcfc03457c849787491037a1d555d088b1591
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2021-03-03 19:01:01 -0500

    Prettier

--- web/client-config.js
+++ web/client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
-    projectId: process.env.GATSBY_SANITY_PROJECT_ID || 'nfy2l0gm',
-    dataset: process.env.GATSBY_SANITY_DATASET || 'production'
-  }
-}
+    projectId: process.env.GATSBY_SANITY_PROJECT_ID || "nfy2l0gm",
+    dataset: process.env.GATSBY_SANITY_DATASET || "production",
+  },
+};
