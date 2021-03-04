d766cd75f01aed4da01255b435e8cfeb5ec93b84
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2021-03-03 19:40:40 -0500

    Add gatsby-plugin-typescript

--- web/gatsby-config.js
+++ web/gatsby-config.js
@@ -21,5 +21,13 @@ module.exports = {
         overlayDrafts: !isProd && token,
       },
     },
+    {
+      resolve: "gatsby-plugin-typescript",
+      options: {
+        isTSX: true, // defaults to false
+        // jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
   ],
 }
